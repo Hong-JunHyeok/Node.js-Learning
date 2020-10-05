1846c9e9e0d95f618cd57beeebc041a47c127410
Hong-JunHyeok <edb1631@naver.com>
2020-10-05 12:16:45 +0900

    node.js http라이브러리 사용

--- hello.js
+++ hello.js
@@ -3,9 +3,9 @@ const http = require("http");
 const hostname = "127.0.0.1";
 const port = 1337;
 
-http.createServer((req, res) => {
+http.createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World\n");
-}).listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    res.end("success!\n");
+}).listen(port, hostname, function () {
+    console.log(`server running at (http://${hostname}:${port}/)`);
 });
