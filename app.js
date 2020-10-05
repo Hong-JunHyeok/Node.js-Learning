60f1e60e8e758e0050df7d15d78835c17f3b271b
Hong-JunHyeok <edb1631@naver.com>
2020-10-05 16:52:14 +0900

    템플릿 엔진 설치

--- app.js
+++ app.js
@@ -5,18 +5,6 @@ app.use(express.static("public"));
 //public을 정적인 파일이 있는곳이라고 하겠다
 //정적인 파일은 다시 서버를 재부팅 하지 않아도 바로 반영된다.
 
-app.get("/dynamic", function (req, res) {
-    let lis = "";
-    for (var i = 0; i < 5; i++) {
-        lis =
-            lis +
-            `
-        <li>
-            coding ${i}
-        </li>`;
-    }
-    res.send(lis);
-});
 //동적인 파일의 경우에는 재부팅을 해야 반영이 된다.(자동화 시키는 툴이 있긴하다)
 app.get("/", function (req, res) {
     res.send("Welcome to / page"); //사용자가 /로 접속을 했을때 Welcome to / page라는 내용을 send함
