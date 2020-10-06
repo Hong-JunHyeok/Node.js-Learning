df065dfe6065ae84dc56177ac441b9cdf6097277
Hong-JunHyeok <edb1631@naver.com>
2020-10-06 09:47:56 +0900

    get방식 연습

--- app.js
+++ app.js
@@ -1,49 +1,31 @@
-const express = require("express");
-var bodyParser = require("body-parser");
+var express = require("express"); //express 를 사용하기 위함
+var bodyParser = require("body-parser"); //POST방식의 데이터를 얻기 위함
+var fs = require("fs"); //파일 관리를 하기 위함
+var app = express(); //express매서드를 사용해서 app객체를 얻음
+var os = require("os"); //os라이브러리를 가져옴 (운영체제의 정보를 얻기 위해)
 
-let app = express();
-app.set("view engine", "jade");
+app.locals.pretty = true; //jade를 이쁘게 보이게 하기 위함
+
 app.set("views", "views");
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.get("/form", function (req, res) {
-    res.render("temp");
+app.set("view engine", "jade"); //jade를 view engine으로 사용
+
+// 라우팅
+app.get("/", function (req, res) {
+    res.send("local server에 오신것을 환영합니다");
+}); // "/" 경로에 대한 라우트 설정
+
+app.get("/topic", function (req, res) {
+    res.render("form");
 });
 
-// app.get("/form_receiver", function (req, res) {
-//     var title = req.query.title;
-//     var discription = req.query.discription;
-//     res.send(title + "," + discription);
-// });
-
-app.post("/form_receiver", function (req, res) {
-    var title = req.body.title;
-    var discription = req.body.discription;
-    res.send(title + "," + discription);
-});
-
-app.get("/topic/:id", function (req, res) {
-    var topics = ["js는 ...이다", "nodejs는 ...이다", "express는 ...이다"];
-    var as = `
-        <a href="/topic/0">JS 설명</a></br>
-        <a href="/topic/1">NodeJs 설명</a></br>
-        <a href="/topic/2">Express 설명</a></br>
-
-        ${topics[req.params.id]}
-    `;
-
-    res.send(as);
-    //ex ) ?id=2
-});
-
-app.get("/topic/:id/:mode", function (req, res) {
-    res.send(req.params.id + "," + req.params.mode);
-});
-
-app.get("/temp", function (req, res) {
-    res.send();
+app.use(bodyParser.urlencoded({ extended: false })); //bodyParser 설정
+app.get("/form_receiver", function (req, res) {
+    var title = req.query.title;
+    var description = req.query.description;
+    res.send("title : " + title + " description : " + description);
 });
 
 app.listen(3000, function () {
-    console.log("Connected 3000 port");
-}); //포트 번호를 지정
+    var osInfo = os.platform();
+    console.log("Connected 3000 port at " + osInfo);
+});
